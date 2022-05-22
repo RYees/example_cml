39d1d02f0935d1b38b821059e0d562847b4af46b
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-05-22 20:49:22 +0300

    depth value changes made

--- train.py
+++ train.py
@@ -1,25 +1,26 @@
-from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import ConfusionMatrixDisplay
-# import matplotlib.pyplot as plt
-import json
-import os
-import numpy as np
+# from sklearn.ensemble import RandomForestClassifier
+# # from sklearn.metrics import ConfusionMatrixDisplay
+# # import matplotlib.pyplot as plt
+# import json
+# import os
+# import numpy as np
 
 # Read in data
-X_train = np.genfromtxt("data/train_features.csv")
-y_train = np.genfromtxt("data/train_labels.csv")
-X_test = np.genfromtxt("data/test_features.csv")
-y_test = np.genfromtxt("data/test_labels.csv")
+# X_train = np.genfromtxt("data/train_features.csv")
+# y_train = np.genfromtxt("data/train_labels.csv")
+# X_test = np.genfromtxt("data/test_features.csv")
+# y_test = np.genfromtxt("data/test_labels.csv")
 
+print("hello everyone")
 # Fit a model
-depth = 2
-clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train, y_train)
+# depth = 2
+# clf = RandomForestClassifier(max_depth=depth)
+# clf.fit(X_train, y_train)
 
-acc = clf.score(X_test, y_test)
-print(acc)
-with open("metrics.txt", "w") as outfile:
-    outfile.write("Accuracy: " + str(acc) + "\n")
+# acc = clf.score(X_test, y_test)
+# print(acc)
+# with open("metrics.txt", "w") as outfile:
+#     outfile.write("Accuracy: " + str(acc) + "\n")
 
 # Plot it
 # disp = ConfusionMatrixDisplay.from_estimator(
